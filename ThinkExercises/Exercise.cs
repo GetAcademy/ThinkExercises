8030c405eb456d2c59a4a68eb4c9b985cf1b2d57
GET Academy <terje@getacademy.no>
2023-01-17 10:51:46 +0100

    Update Exercise.cs

--- ThinkExercises/Exercise.cs
+++ ThinkExercises/Exercise.cs
@@ -22,85 +22,135 @@
                 2. Hashtag - starter på 1 - øker med 1
                 3. Space - starter på 6 - minker med 2        
              */
-            var spaceCount1 = 0;
-            var hashCount = 1;
-            var spaceCount2 = 6;
+            WriteFourRows(0, 1, 6,
+                1, 1, -2);
+            WriteFourRows(3, 4, 0,
+                -1, -1, 2);
+        }
 
+        private static void WriteFourRows(
+            int spaceCount1, int hashCount, int spaceCount2,
+            int changeSpaceCount1, int changeHashCount, int changeSpaceCount2
+            )
+        {
             for (var lineIndex = 0; lineIndex < 4; lineIndex++)
             {
-                for (var i = 0; i < spaceCount1; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < hashCount; i++)
-                {
-                    WriteHash();
-                }
-                for (var i = 0; i < spaceCount2; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < spaceCount2; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < hashCount; i++)
-                {
-                    WriteHash();
-                }
-                for (var i = 0; i < spaceCount1; i++)
-                {
-                    WriteSpace();
-                }
-                spaceCount1++;
-                hashCount++;
-                spaceCount2 -= 2;
+                WriteRow(spaceCount1, hashCount, spaceCount2);
+                spaceCount1 += changeSpaceCount1;
+                hashCount += changeHashCount;
+                spaceCount2 += changeSpaceCount2;
                 Console.WriteLine();
             }
+        }
 
-            for (var lineIndex = 3; lineIndex >= 0; lineIndex--)
+        private static void WriteRow(int spaceCount1, int hashCount, int spaceCount2)
+        {
+            WriteSpacesHashesSpaces(spaceCount1, hashCount, spaceCount2);
+            WriteSpacesHashesSpaces(spaceCount2, hashCount, spaceCount1);
+        }
+
+        private static void WriteSpacesHashesSpaces(int spaceCount1, int hashCount, int spaceCount2)
+        {
+            WriteCharacters(spaceCount1, ' ');
+            WriteCharacters(hashCount, '#');
+            WriteCharacters(spaceCount2, ' ');
+        }
+
+        private static void WriteCharacters(int count, char character)
+        {
+            for (var i = 0; i < count; i++)
             {
-                spaceCount1--;
-                hashCount--;
-                spaceCount2 += 2;
-                for (var i = 0; i < spaceCount1; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < hashCount; i++)
-                {
-                    WriteHash();
-                }
-                for (var i = 0; i < spaceCount2; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < spaceCount2; i++)
-                {
-                    WriteSpace();
-                }
-                for (var i = 0; i < hashCount; i++)
-                {
-                    WriteHash();
-                }
-                for (var i = 0; i < spaceCount1; i++)
-                {
-                    WriteSpace();
-                }
-
-                Console.WriteLine();
+                Console.Write(character);
             }
-
         }
 
-        private static void WriteHash()
-        {
-            Console.Write("#");
-        }
+        //public static void No2_2_v1()
+        //{
+        //    /*
+        //        2-3 Here’s an especially tricky one:
+        //        #            #
+        //         ##        ##
+        //          ###    ###
+        //           ########
+        //           ########
+        //          ###    ###
+        //         ##        ##
+        //        #            #             
 
-        private static void WriteSpace()
-        {
-            Console.Write(" ");
-        }
+        //        fire første linjer til midten
+        //        1. Space - starter på 0 - øker med 1
+        //        2. Hashtag - starter på 1 - øker med 1
+        //        3. Space - starter på 6 - minker med 2        
+        //     */
+        //    var spaceCount1 = 0;
+        //    var hashCount = 1;
+        //    var spaceCount2 = 6;
+
+        //    for (var lineIndex = 0; lineIndex < 4; lineIndex++)
+        //    {
+        //        for (var i = 0; i < spaceCount1; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < hashCount; i++)
+        //        {
+        //            WriteCharacters();
+        //        }
+        //        for (var i = 0; i < spaceCount2; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < spaceCount2; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < hashCount; i++)
+        //        {
+        //            WriteCharacters();
+        //        }
+        //        for (var i = 0; i < spaceCount1; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        spaceCount1++;
+        //        hashCount++;
+        //        spaceCount2 -= 2;
+        //        Console.WriteLine();
+        //    }
+
+        //    for (var lineIndex = 3; lineIndex >= 0; lineIndex--)
+        //    {
+        //        spaceCount1--;
+        //        hashCount--;
+        //        spaceCount2 += 2;
+        //        for (var i = 0; i < spaceCount1; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < hashCount; i++)
+        //        {
+        //            WriteCharacters();
+        //        }
+        //        for (var i = 0; i < spaceCount2; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < spaceCount2; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+        //        for (var i = 0; i < hashCount; i++)
+        //        {
+        //            WriteCharacters();
+        //        }
+        //        for (var i = 0; i < spaceCount1; i++)
+        //        {
+        //            WriteSpaces();
+        //        }
+
+        //        Console.WriteLine();
+        //    }
+
+        //}
     }
 }
